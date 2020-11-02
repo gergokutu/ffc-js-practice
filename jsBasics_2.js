6acda22b93f0526718ff46a9e0b879d2938c1c5f
gergokutu <gergo.kutu@gmail.com>
2020-11-02 17:20:32 +0100

    Access the first item of the array

--- jsBasics_2.js
+++ jsBasics_2.js
@@ -1,2 +1,7 @@
 var myArray = ['string', 0];
-var myArray = [ ['string', 0], ['string2', 1] ];
+var myArray = [['string', 0], ['string2', 1]];
+
+{
+  var myArray = [50, 60, 70];
+  var myData = myArray[0];
+}
