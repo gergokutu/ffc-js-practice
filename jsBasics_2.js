aa117d87a46d0a6b082b8af8ef0ba991ec56197b
gergokutu <gergo.kutu@gmail.com>
2020-11-02 17:25:18 +0100

    Use array method > push()

--- jsBasics_2.js
+++ jsBasics_2.js
@@ -11,3 +11,8 @@ var myArray = [['string', 0], ['string2', 1]];
   var myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 14]];
   var myData = myArray[2][1]; // 8
 }
+
+{
+  var myArray = [["John", 23], ["cat", 2]];
+  myArray.push(["dog", 3]);
+}
