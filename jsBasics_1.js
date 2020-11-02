24163e4fb098d8064a2fdd3e744e1c84d1df5b75
gergokutu <gergo.kutu@gmail.com>
2020-11-02 15:48:26 +0100

    Use camleCase

--- jsBasics_1.js
+++ jsBasics_1.js
@@ -11,8 +11,19 @@ a = 7;
 var b;
 b = a;
 
+// Initialization
 var c = 9;
 
 // d is not defined
 var e;
 console.log(e) // e is undefined
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
