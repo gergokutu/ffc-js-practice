9eaeba630ec6390d63c5358c305c2e8ce8ae83fb
gergokutu <gergo.kutu@gmail.com>
2020-11-02 17:37:00 +0100

    Implement shopping list with array.push()

---
+++ shoppingList.js
@@ -0,0 +1,9 @@
+var myList = [];
+myList.push(
+    ["Chocolate Bar 1", 15],
+    ["Chocolate Bar 2", 20],
+    ["Chocolate Bar 3", 25],
+    ["Chocolate Bar 4", 30],
+    ["Chocolate Bar 5", 35]
+);
+console.log('You should by these:', myList);
