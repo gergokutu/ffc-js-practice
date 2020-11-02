834ee43f4c2f59c088a7f5a86b412fa90625a4ba
gergokutu <gergo.kutu@gmail.com>
2020-11-02 17:12:44 +0100

    Implement wordBlanks

---
+++ wordBlanks.js
@@ -0,0 +1,6 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+var wordBlanks = "I have a " + myNoun + " with " + myAdjective + " eyes and you can " + myVerb + " away " + myAdverb + ".";
+console.log(wordBlanks);
