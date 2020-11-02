acc36a1472be1b4cf7c9b9e4656528fb05f83dda
gergokutu <gergo.kutu@gmail.com>
2020-11-02 15:51:14 +0100

    Use basic arithmetic operators

--- jsBasics_1.js
+++ jsBasics_1.js
@@ -27,3 +27,8 @@ var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
+
+var sum = 10 + 10;
+var difference = 45 - 33;
+var product = 8 * 10;
+var quotient = 66 / 33;
