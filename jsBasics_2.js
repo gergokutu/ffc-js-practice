a9364a3a1056730e5bd5e038ab688ac1dd4d51b6
gergokutu <gergo.kutu@gmail.com>
2020-11-02 17:23:35 +0100

    Acces multidimensional array with indexes

--- jsBasics_2.js
+++ jsBasics_2.js
@@ -6,3 +6,8 @@ var myArray = [['string', 0], ['string2', 1]];
   var myData = myArray[0];
   myArray[0] = 45;
 }
+
+{
+  var myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 14]];
+  var myData = myArray[2][1]; // 8
+}
