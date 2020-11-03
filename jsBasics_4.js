d9d971a76395d90afe91b62bf0484f24996c9d9d
gergokutu <gergo.kutu@gmail.com>
2020-11-03 13:44:18 +0100

    Use the strict equality operator

--- jsBasics_4.js
+++ jsBasics_4.js
@@ -6,3 +6,12 @@ function trueOrFalse(wasThatTrue) {
 }
 const thisIsTrue = trueOrFalse(true);
 console.log('Test should be > Yes, that was true\nResult:', thisIsTrue);
+
+function testEqual(val, val2) {
+  if (val === val2) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+console.log('Should not be equal >', testEqual(10, '10'));
