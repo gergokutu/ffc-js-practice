2d84841a588f6f07b5b321120096301cd41fc461
gergokutu <gergo.kutu@gmail.com>
2020-11-03 08:41:24 +0100

    Global scope

--- jsBasics_3.js
+++ jsBasics_3.js
@@ -7,3 +7,22 @@ function functionWithArgs(num1, num2) {
   console.log(num1 + num2);
 }
 functionWithArgs(6, 8);
+
+// Declare the myGlobal variable below this line
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
