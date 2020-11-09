5a71a1d4e8a8f0ecd9aaeb027e163348822afeeb
gergokutu <gergo.kutu@gmail.com>
2020-11-09 10:52:15 +0100

    Implement Blacj Jack card counter

---
+++ countingCards.js
@@ -0,0 +1,23 @@
+var count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  return count < 1 ? count + " Hold" : count + " Bet";
+}
+console.log("Should return: 0 Hold >", cc(7));
