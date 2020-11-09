c59430a4c667efa6897faf231be0283152a6282b
gergokutu <gergo.kutu@gmail.com>
2020-11-09 10:07:47 +0100

    Apply default in a switch statement

--- jsBasics_5.js
+++ jsBasics_5.js
@@ -18,5 +18,26 @@ function caseInSwitch(val) {
 
   return answer;
 }
-
 console.log("Should return: alpha >", caseInSwitch(1));
+
+function switchOfStuff(val) {
+  var answer = "";
+
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+  }
+
+  return answer;
+}
+
+console.log("Should return: stuff >", switchOfStuff(1));
