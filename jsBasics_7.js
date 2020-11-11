26dbc51cab0f0f1b622bcf405b2fecf825107872
gergokutu <gergo.kutu@gmail.com>
2020-11-11 16:18:18 +0100

    Implement a for loop

--- jsBasics_7.js
+++ jsBasics_7.js
@@ -6,3 +6,12 @@ while (i >= 0) {
   i--;
 }
 console.log("Numbers 5 through 0(inclusive):", myArray);
+
+// Setup
+var myArray2 = [];
+
+// Only change code below this line
+for (var i = 1; i < 6; i++) {
+  myArray2.push(i);
+}
+console.log("Numbers 1 through 6(exclusive):", myArray2);
