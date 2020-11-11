0774e12c18a139f9470212bfdfa14b57c540c4e9
gergokutu <gergo.kutu@gmail.com>
2020-11-11 10:14:16 +0100

    Access with bracket notation

--- jsBasics_6.js
+++ jsBasics_6.js
@@ -10,6 +10,15 @@ var testObj = {
   "shirt": "jersey",
   "shoes": "cleats"
 };
-
 var hatValue = testObj.hat;
-var shirtValue = testObj.shirt;   
+var shirtValue = testObj.shirt;
+
+
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+var entreeValue = testObj["an entree"];
+var drinkValue = testObj["the drink"];
+
