1ac7b881de61951520dcf131c59ddb74cd21b13a
gergokutu <gergo.kutu@gmail.com>
2020-11-11 16:24:29 +0100

    Iterate over an array with for...of

--- jsBasics_7.js
+++ jsBasics_7.js
@@ -7,11 +7,18 @@ while (i >= 0) {
 }
 console.log("Numbers 5 through 0(inclusive):", myArray);
 
-// Setup
 var myArray2 = [];
 
-// Only change code below this line
 for (var i = 1; i < 6; i++) {
   myArray2.push(i);
 }
 console.log("Numbers 1 through 6(exclusive):", myArray2);
+
+var myArr = [2, 3, 4, 5, 6];
+
+var total = 0;
+
+for (var item of myArr) {
+  total += item;
+}
+console.log("total should be 20 >", total);
