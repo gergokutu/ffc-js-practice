83927dc35ab6fc69ce6d8970a34c5440b69027e1
gergokutu <gergo.kutu@gmail.com>
2020-11-13 09:56:28 +0100

    String to number

--- jsBasics_8.js
+++ jsBasics_8.js
@@ -7,3 +7,13 @@ function randomWholeNum() {
 function randomRange(myMin, myMax) {
   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
 }
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+console.log("Should be 56 (number) >", convertToInteger("56"));
+
+function convertToInteger(str, radix) {
+  return parseInt(str, radix);
+}
+console.log("Should return: 19 > ", convertToInteger("10011", 2));
