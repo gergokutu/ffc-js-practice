ea23eaf6e5afabda8793d01057ccdabed9d0709c
gergokutu <gergo.kutu@gmail.com>
2020-11-13 09:50:21 +0100

    Generate random whole number in a given range

--- jsBasics_8.js
+++ jsBasics_8.js
@@ -2,3 +2,8 @@
 function randomWholeNum() {
   return Math.floor(Math.random() * 10);
 }
+
+// Random whole number in a range
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
