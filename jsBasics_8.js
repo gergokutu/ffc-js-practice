86917ea129b4e759ccd0fc4108d8e3acca6d58b2
gergokutu <gergo.kutu@gmail.com>
2020-11-13 10:06:57 +0100

    Use ternary operators

--- jsBasics_8.js
+++ jsBasics_8.js
@@ -17,3 +17,15 @@ function convertToInteger(str, radix) {
   return parseInt(str, radix);
 }
 console.log("Should return: 19 > ", convertToInteger("10011", 2));
+
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
+}
+console.log("Should return: Not Equal >", checkEqual(1, 2));
+
+function checkSign(num) {
+  return !num ? "zero"
+    : num > 0 ? "positive"
+      : "negative";
+}
+console.log("10 should return: positive >", checkSign(10));
