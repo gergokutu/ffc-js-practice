5f78f4ba05364acf02a36ead57bd8e412a885c27
gergokutu <gergo.kutu@gmail.com>
2021-11-13 17:41:22 +0100

    Implement Ver2 countingCards - RegExp with return ternary

--- countingCards.js
+++ countingCards.js
@@ -1,23 +1,38 @@
 var count = 0;
 
+// // Ver1
+// function cc(card) {
+//   switch (card) {
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//       count++;
+//       break;
+//     case 10:
+//     case "J":
+//     case "Q":
+//     case "K":
+//     case "A":
+//       count--;
+//       break;
+//   }
+
+//   return count < 1 ? count + " Hold" : count + " Bet";
+// }
+
+// Ver2
 function cc(card) {
-  switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-      break;
+  let myRegex = new RegExp('[JQKA]');
+
+  if (card > 1 && card < 7) {
+    count++;
+  } else if (card === 10 || myRegex.test(card)) {
+    count--;
   }
 
-  return count < 1 ? count + " Hold" : count + " Bet";
+  return count < 1 ? `${count} Hold` : `${count} Bet`;
 }
+
 console.log("Should return: 0 Hold >", cc(7));
